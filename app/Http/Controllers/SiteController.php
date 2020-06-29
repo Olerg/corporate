8b2e5abfdc3b07d7e9808b51dd9646174bae1afc
Olerg <schara@mail.ru>
2020-06-29 20:36:43 +0300

    Отображение главного меню на экран

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace Corp\Http\Controllers;
 
 use Corp\Repositories\MenusRepository;
 use Illuminate\Http\Request;
+use Menu;
 
 
 class SiteController extends Controller
@@ -26,12 +27,25 @@ class SiteController extends Controller
 
         $menu = $this->getMenu();
 
-        $navigation = view(env('THEME').'.navigation')->render();
+        $navigation = view(env('THEME').'.navigation')->with('menu', $menu)->render();
         $this->vars = array_add($this->vars,'navigation',$navigation);
         return view($this->template)->with($this->vars);
     }
     protected function getMenu(){
         $menu = $this->m_rep->get();
-        return $menu;
+
+        $mBuilder = Menu::make('MyNav', function ($m) use ($menu){
+            foreach ($menu as $item){
+                if($item->parent == 0){
+                    $m->add($item->title, $item->path)->id($item->id);
+                }else{
+                    if($m->find($item->parent)){
+                        $m->find($item->parent)->add($item->title,$item->path)->id($item->id);
+                    }
+                }
+            }
+        });
+
+        return $mBuilder;
     }
 }
