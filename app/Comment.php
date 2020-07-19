795fc87eab02416a706666e284ca3f7886cb34c7
Olerg <schara@mail.ru>
2020-07-19 10:53:22 +0300

    Сохранение нового комментария в базу данных

--- app/Comment.php
+++ app/Comment.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    public function article(){
+    protected $fillable = ['name','text','site','user_id','article_id','parent_id','email'];
+
+    public function article() {
         return $this->belongsTo('Corp\Article');
     }
-    public function user(){
+
+    public function user() {
         return $this->belongsTo('Corp\User');
     }
 }
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,12 @@
 
 namespace Corp\Http\Controllers;
 
+use Corp\Comment;
+use Validator;
 use Illuminate\Http\Request;
+use Auth;
+use Response;
+use Corp\Article;
 
 class CommentController extends SiteController
 {
@@ -34,9 +39,29 @@ class CommentController extends SiteController
      */
     public function store(Request $request)
     {
-        echo json_encode(['hello'=>'world']);
-        exit;
+        $data = $request->except('_token','comment_post_ID','comment_parent');
+        $data['article_id'] = $request->input('comment_post_ID');
+        $data['parent_id'] = $request->input('comment_parent');
+        $validator = Validator::make($data,[
+            'article_id'=>'integer|required',
+            'parent_id'=>'integer|required',
+            'text'=>'string|required'
+        ]);
+        $validator->sometimes(['name','email'],'required|max:255',function ($input){
+            return !Auth::check();
+        });
+        if($validator->fails()){
+            return Response::json(['error'=>$validator->errors()->all()]);
+        }
+        $user = Auth::user();
+        $comment = new Comment($data);
+        if($user){
+            $comment->user_id = $user->id;
+        }
+        $post = Article::find($data['article_id']);
+        $post->comments()->save($comment);
     }
+
 
     /**
      * Display the specified resource.
