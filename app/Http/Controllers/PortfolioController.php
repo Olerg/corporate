562252642a60883e3384ccbdc32cbe308a26bef1
Olerg <schara@mail.ru>
2020-07-23 19:24:03 +0300

    Страница детального просмотра работы портфолио

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -29,11 +29,24 @@ class PortfolioController extends SiteController
         return $this->renderOutput();
     }
 
-    public function getPortfolios(){
-        $portfolios = $this->p_rep->get('*',false,true);
+    public function getPortfolios($take = false, $paginate = true){
+        $portfolios = $this->p_rep->get('*',$take,$paginate);
         if($portfolios){
             $portfolios->load('filter');
         }
         return $portfolios;
+    }
+
+    public function show($alias){
+
+        $portfolio = $this->p_rep->one($alias);
+        $portfolios = $this->getPortfolios(config('settings.other_portfolios'),false);
+        $this->title = $portfolio->title;
+        $this->keywords = $portfolio->keywords;
+        $this->meta_desc = $portfolio->meta_desc;
+        $content = view(env('THEME').'.portfolio_content')->with(['portfolio'=>$portfolio, 'portfolios'=>$portfolios])->render();
+        $this->vars = Arr::add($this->vars,'content', $content);
+
+        return $this->renderOutput();
     }
 }
