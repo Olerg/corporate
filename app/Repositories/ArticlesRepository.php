ae73c4c2b6db8cbc695214db85b17d6f85fcd0cd
Olerg <schara@mail.ru>
2020-08-17 20:41:04 +0300

    Сохранение нового материала в базу данных. Часть 2

--- app/Repositories/ArticlesRepository.php
+++ app/Repositories/ArticlesRepository.php
@@ -2,6 +2,8 @@
 namespace Corp\Repositories;
 use Corp\Article;
 use Gate;
+use Image;
+use Illuminate\Support\Str;
 
 class ArticlesRepository extends Repository{
     public function __construct(Article $articles)
@@ -33,7 +35,37 @@ class ArticlesRepository extends Repository{
             $data['alias'] = $this->transliterate($data['title']);
         }
 
-        dd($data);
+        if($this->one($data['alias'], false)){
+            $request->merge(array('alias'=>$data['alias']));
+            $request->flash();
+
+            return ['error' => 'Данный псевдоним уже используется'];
+        }
+
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+
+            if($image->isValid()){
+                $str = Str::random(8);
+                $obj = new \stdClass;
+                $obj->mini = $str.'_mini.jpg';
+                $obj->max = $str.'_max.jpg';
+                $obj->path = $str.'.jpg';
+
+                $img = Image::make($image);
+
+                $img->fit(\Config::get('settings.image')['width'],\Config::get('settings.image')['height'])->save(public_path().'/'.env('THEME').'/images/articles/'.$obj->path);
+                $img->fit(\Config::get('settings.articles_img')['max']['width'],\Config::get('settings.articles_img')['max']['height'])->save(public_path().'/'.env('THEME').'/images/articles/'.$obj->max);
+                $img->fit(\Config::get('settings.articles_img')['mini']['width'],\Config::get('settings.articles_img')['mini']['height'])->save(public_path().'/'.env('THEME').'/images/articles/'.$obj->mini);
+
+                $data['img'] = json_encode($obj);
+                $this->model->fill($data);
+
+                if($request->user()->articles()->save($this->model)){
+                    return ['status' => 'Материал добавлен'];
+                }
+            }
+        }
 
     }
 }
